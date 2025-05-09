0668540fd4321a7cda049567725958373a03c9c8
Abubeker Assefa <realabubekerassefa@gmail.com>
2025-05-09 18:32:55 +0300

    add signup route

--- route.php
+++ route.php
@@ -8,6 +8,11 @@ switch ($request) {
     case '/home' :
         require __DIR__ . '/Controller/home.php';
         break;
+
+    case '/signup' :
+        require __DIR__ . '/Controller/signup.php';
+        break;
+
     default:
         http_response_code(404);
         require __DIR__ . '/View/404.view.php';
