31a94c13de0750ea02cc22ab941638b88b7f6695
hawi3075 <hawig3266@gmail.com>
2025-05-09 21:14:22 +0300

    added construct function

--- Database.php
+++ Database.php
@@ -5,5 +5,13 @@ class Database {
     private $pdo;
     private $stmt;
 
-
+    public function __construct() {
+        try {
+            $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4";
+            $this->pdo = new PDO($dsn, DB_USER, DB_PASS);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("DB Connection failed: " . $e->getMessage());
+        }
+    }
 }
