cce88ff2b62d169f56063aa05201df46d966f2d6
Abubeker Assefa <realabubekerassefa@gmail.com>
2025-05-09 18:32:01 +0300

    add home route

--- route.php
+++ route.php
@@ -5,6 +5,9 @@ $request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
     case '/' :
+    case '/home' :
+        require __DIR__ . '/Controller/home.php';
+        break;
     default:
         http_response_code(404);
         require __DIR__ . '/View/404.view.php';
